99051c704f82efa8615b1471c96f540c377923c6
Mell Lessington <59120468+mjlessington@users.noreply.github.com>
2020-08-11 18:13:12 -0400

    section window complete, chat window in process

--- chit_chat/src/components/MainChat.js
+++ chit_chat/src/components/MainChat.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react'
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Chip from '@material-ui/core/Chip';
 
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      padding: theme.spacing(3,2)
+        margin: '50px',
+        padding: theme.spacing(3,2)
+    },
+    flex: {
+        display: 'flex'
+    },
+    sectionWindow: {
+        width: '30%',
+        height: '300px',
+        borderRight: '1px solid black'
+
+    },
+    chatWindow: {
+        width: '70%',
+        height: '300px'
+
+    },
+    chatBox: {
+        width: '85%'
+
+    },
+    button: {
+        width:'15%'
+
     },
   }));
   
@@ -15,18 +43,52 @@ const useStyles = makeStyles((theme) => ({
     const classes = useStyles();
   
     return (
-      
-        <Paper className={classes.root} elevation={3}/>
-            <Typography variant="h5" gutterBottom>
-                Test
+     <div>
+        <Paper className={classes.root} elevation={3}>
+            <Typography variant="h4" component="h4">
+                Chit Chat
             </Typography>
-            <Typography variant="body1" gutterBottom>
-            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-            dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            <Typography variant="h6" gutterBottom>
+                Chat Room
             </Typography>
+            <Divider />
+            <div className={classes.flex}>
+                <div className={classes.sectionWindow}>
+                    <List>
+                        {
+                            [].map(section => (
+                                <div className= {classes.flex}>
 
-      
+
+                                </div>
+
+                            ))
+                        }
+                    </List>
+
+                </div>
+                <div className={classes.chatWindow}>
+                
+                        {
+                            [{from:'user', msg: 'Welcome'}].map((chat, i) => (
+                                <div className={classes.flex} key={}>
+                                <Chip label="Basic" />
+
+                            ))
+                        }
+                
+
+                </div>
+                
+
+            </div>
+            <div className={classes.flex}>
+
+
+            </div>
+        </Paper> 
+
+      </div>
     );
   }
 
